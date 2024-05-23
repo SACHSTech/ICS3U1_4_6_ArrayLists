17cb7812205f131a82d8962f528515ebe0bf4e39
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2024-05-23 11:19:30 -0400

    Create Program1.java

---
+++ Program1.java
@@ -0,0 +1,47 @@
+
+
+class Program1 extends ConsoleProgram {
+
+  /**
+  * Description
+  * @author:
+  */
+  
+  public void run() {
+    
+    // create an ArrayList object
+   
+    // add items
+ 
+
+    // print the arrayList
+   
+    // access items
+  
+    
+    // change items
+    
+    
+    // remove items
+   
+
+    
+    // add to specific position
+    
+    
+    
+    // length of ArrayList
+   
+
+    
+    // iteration
+
+
+    // for each style
+
+    
+
+    // sorting
+
+  }
+}
